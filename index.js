650f56b7d90e1dab95274ad29db67a4bf520edc1
krishan kant sharma <kk41110002@gmail.com>
2022-08-03 10:47:46 +0530

    ready to deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("./config/mongoose");
+const port = 8000;
+app.use(express.urlencoded());
+
+app.use("/", require("./router"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("server is running on port :", port);
+});
